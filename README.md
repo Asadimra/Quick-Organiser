1f2fd57ad38284f2c8d9bbf3131f3f2c3c1a8a73
Asadimra <asadimran031@gmail.com>
2024-03-31 21:09:52 +0500

    uploading the code

---
+++ README.md
@@ -0,0 +1,3 @@
+"# Quick-Organiser" 
+"# Quick-Organiser" 
+"# Quick-Organiser" 
---
+++ main.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let askadd = await inquirer.prompt([
+        {
+            name: "add",
+            type: "input",
+            message: "What you want to add in your Todos?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more:",
+            default: "false",
+        },
+    ]);
+    todos.push(askadd.add);
+    condition = askadd.addMore;
+    //console.log(todos);
+    let dcut = await inquirer.prompt([
+        {
+            name: "delete",
+            type: "confirm",
+            message: "Do you delete this item",
+            default: "false",
+        },
+    ]);
+    if (dcut.delete == true) {
+        todos.pop();
+    }
+    console.log(todos);
+}
