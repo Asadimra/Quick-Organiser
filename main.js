f2f1d02f66038f8379495d096c417ba075232250
Asadimra <asadimran031@gmail.com>
2024-04-03 07:04:50 +0500

    create new feature add and review

--- main.js
+++ main.js
@@ -10,13 +10,57 @@ while (condition) {
             message: "What you want to add in your Todos?",
         },
         {
-            name: "addMore",
+            name: "delete",
             type: "confirm",
-            message: "Do you want to add more:",
+            message: "Do you want to delete this item:",
             default: "false",
         },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more item in your todos?:",
+            default: "false",
+        }
     ]);
     todos.push(askadd.add);
     condition = askadd.addMore;
-    console.log(todos);
+    if (askadd.delete == true) { // DELETE OPTION IS AVAILABLE
+        todos.pop();
+        console.log(todos);
+    }
+    else {
+        console.log(todos);
+    }
+}
+let question = await inquirer.prompt([
+    {
+        name: "questionask",
+        type: "list",
+        message: "Select one of this:",
+        choices: ["Read your todos", "Adding in your todos"], //READ YOUR TODO AND ADD SOMETHING IN YOUR TODO
+    },
+]);
+if (question.questionask == "Read your todos") {
+    console.log("Your todos list is this:" + (todos));
+}
+let condition1 = true;
+if (question.questionask == "Adding in your todos") {
+    while (condition1) {
+        let adding = await inquirer.prompt([
+            {
+                name: "addingtask",
+                type: "input",
+                message: "What you want to add in your todos:",
+            },
+            {
+                name: "addMore1",
+                type: "confirm",
+                message: "Do you want to add more item in your todos?:",
+                default: "false",
+            },
+        ]);
+        todos.push(adding.addingtask);
+        console.log(todos);
+        condition1 = adding.addMore1;
+    }
 }
--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 import inquirer from "inquirer";
 let todos = [];
 let condition = true;
+
 while (condition) {
   let askadd = await inquirer.prompt([
     {
@@ -10,16 +11,65 @@ while (condition) {
       message: "What you want to add in your Todos?",
     },
     {
-      name: "addMore",
+      name: "delete",
       type: "confirm",
-      message: "Do you want to add more:",
+      message: "Do you want to delete this item:",
       default: "false",
     },
+    {
+      name: "addMore",
+      type:"confirm",
+      message:"Do you want to add more item in your todos?:",
+      default:"false",
+    }
    
   ]);
   todos.push(askadd.add);
-  condition = askadd.addMore  ;
- 
-    console.log(todos);
-
+  condition = askadd.addMore ;
+if (askadd.delete==true){  // DELETE OPTION IS AVAILABLE
+  todos.pop()
+  console.log(todos);}
+  else {
+    console.log(todos);    
+  } 
 }
+
+let question = await inquirer.prompt([
+  {
+    name: "questionask",
+    type: "list",
+    message: "Select one of this:",
+    choices: ["Read your todos", "Adding in your todos"],//READ YOUR TODO AND ADD SOMETHING IN YOUR TODO
+  },
+]);
+if (question.questionask == "Read your todos") {
+ 
+    console.log("Your todos list is this:"+(todos));
+  }
+
+let condition1 = true;
+if (question.questionask == "Adding in your todos") {
+  while (condition1) {
+    let adding = await inquirer.prompt([
+      {
+        name: "addingtask",
+        type: "input",
+        message: "What you want to add in your todos:",
+      },
+      {
+        name: "addMore1",
+        type: "confirm",
+        message: "Do you want to add more item in your todos?:",
+        default: "false",
+      },
+    ]);
+    todos.push(adding.addingtask);
+    console.log(todos);
+    condition1 = adding.addMore1;
+  }
+}
+
+
+
+
+
